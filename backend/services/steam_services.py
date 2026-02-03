b3dace0607c189ba7708169d18a1243b9d44afec
Aedan <anmagsom@uci.edu>
2026-02-02 16:32:02 -0800

    SteamID improvement
    Added additional API call to map custom steam url -> 17 digit ID

--- backend/services/steam_services.py
+++ backend/services/steam_services.py
@@ -8,6 +8,33 @@ class SteamService:
         self.base_url = 'https://api.steampowered.com'
     
     def get_owned_games(self, steam_id):
+        try:
+            # Say the user inputed an ID 765XXX..
+            _ = int(steam_id)
+        except ValueError:
+            # Assume the user has entered a string
+            # https://partner.steamgames.com/doc/webapi/isteamuser
+            endpoint = f"https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/"
+            params = {
+                "key": self.api_key,
+                "vanityurl": steam_id,
+                "url_type": 1,
+            }
+
+            response = requests.get(endpoint, params=params)
+            response.raise_for_status() 
+
+            data = response.json()
+        
+            # Success 42 = Fail, 1 = OK
+            match data["response"].get("success", 42):
+                case 1: pass
+                case 42: raise Exception("Not provided a valid steam ID. Please enter your " \
+                                         "custom ID or your 17-digit permanent account " \
+                                         "numeric identifier")
+            
+            steam_id = data["response"]["steamid"]
+
         endpoint = f'{self.base_url}/IPlayerService/GetOwnedGames/v1/'
         params = {
             'key': self.api_key,
